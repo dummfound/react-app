a7577c2243ca8367b25c987aa23646f954edf5b2
Andrey Chesnokov <dummfound@gmail.com>
2022-07-01 20:54:34 +0300

    edit index.js

--- src/index.js
+++ src/index.js
@@ -3,6 +3,18 @@ import reactDom from "react-dom";
 import "bootstrap/dist/css/bootstrap.css"
 
 const element = <h1>Andrey</h1>;
+const getBody = document.querySelector('body');
+
+getBody.addEventListener('click', (event) => {
+    const { target } = event;
+    target.textContent = 'Andrey Chesnokov';
+     setTimeout(() => {
+        target.style.color = 'red';
+        target.style.fontSize = '130px';
+     }, 2000);
+     
+     
+});
 
 reactDom.render(element, document.getElementById('root'));
 
